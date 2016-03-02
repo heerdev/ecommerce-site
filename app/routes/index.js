785206ad002154fe14bc551a310c4893e928a894
Jeffrey Ruder and Midori Bowen <ruderjt@gmail.com>
2016-03-02 11:59:23 -0800

    Add splash page styling.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash({
+      mainProduct: this.store.findRecord('product', '1'),
+      otherProducts: this.store.findAll('product')
+    });
+  }
 });
--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'ecommerce-store',
     environment: environment,
     contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
-    firebase: 'https://YOUR-FIREBASE-NAME.firebaseio.com/',
+    firebase: 'https://ecommerce-store.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
