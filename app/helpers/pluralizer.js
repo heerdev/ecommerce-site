eb4233184744864f2dfd631bce893c362c50285d
Jeffrey Ruder and Midori Bowen <ruderjt@gmail.com>
2016-03-02 17:01:12 -0800

    Fix pluralizer helper

--- app/helpers/pluralizer.js
+++ app/helpers/pluralizer.js
@@ -1,7 +1,9 @@
 import Ember from 'ember';
 
 export function pluralizer(params) {
-  if (params[0] > 1) {
+  if (params[1] === "Box" && params[0] > 1) {
+    return "Boxes";
+  } else if (params[0] > 1) {
     return params[1] + 's';
   } else {
     return params[1];
