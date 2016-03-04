871f076219a6f220dd53b74f0bd469e42923a9c2
Jeffrey Ruder and Midori Bowen <ruderjt@gmail.com>
2016-03-03 16:21:17 -0800

    Add checkout form with alert.

--- app/components/display-shopping-cart.js
+++ app/components/display-shopping-cart.js
@@ -2,4 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
+
+  checkout: false,
+  stuffInCart: Ember.computed(function() {
+    return this.get("shoppingCart.items").length;
+  }),
+
+  actions: {
+    checkout() {
+      this.set("checkout", true);
+    },
+
+    submitOrder() {
+      var name = this.get("name");
+      alert("Thanks for your order, " + name + "! We'll deliver your order for FREE on Tuesday!");
+    }
+  }
 });
